97a0de1ddb9bbd463c2b054b5ccd28b16ac826f9
miriSilverman <mirisboom@gmail.com>
2020-12-19 23:57:30 +0200

    trying stuff

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    private Rigidbody _rb;
+    
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        
+        _rb.AddForce(movement*speed);
+    }
+}
---
+++ Assets/Scripts/Turning.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Turning : MonoBehaviour
+{
+    [SerializeField] private GameObject room;
+    private Ease _moveEase = Ease.Linear;
+    [SerializeField] private int numOfCube;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        DOTween.Init();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // room.transform.DORotate(new Vector3(0, 90, 0), 0.3f);
+
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            switch (numOfCube)
+            {
+                case 1:
+                    room.transform.DORotate(new Vector3(0, 0, 90), 10f);
+                    break;
+                case 2:
+                    room.transform.DORotate(new Vector3(0, 0, -90), 10f);
+                    break;
+
+            }
+        }
+    }
+}
