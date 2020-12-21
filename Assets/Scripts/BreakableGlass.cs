1acfb1b284f4cfedf51b1e6c598edf6909a68ab5
kfir <kfir.hayman@gmail.com>
2020-12-21 14:28:34 +0200

    before pull

---
+++ Assets/Scripts/BreakableGlass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableGlass : MonoBehaviour
+{
+    public float forceThreshold;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+        float collisionForce = other.impulse.magnitude / Time.fixedDeltaTime;
+        Debug.Log("glass hit with force" + collisionForce);
+
+        if (collisionForce > forceThreshold)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -14,7 +14,7 @@ public class Room : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //rb.centerOfMass = transform.position;
     }
 
     // Update is called once per frame
