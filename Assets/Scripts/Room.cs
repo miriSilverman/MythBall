8ae9ec0c02f8495d105aaa0fafa9c332d70e489b
kfir <kfir.hayman@gmail.com>
2020-12-21 19:12:10 +0200

    z axis movement added

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -37,12 +37,30 @@ public class Room : MonoBehaviour
     {
         if (_dragging)
         {
-
             float x = Input.GetAxis("Mouse X") * rotationSpeed;
             float y = Input.GetAxis("Mouse Y") * rotationSpeed;
-            
-            rb.AddTorque(Vector3.down * x, ForceMode.Impulse);
-            rb.AddTorque(Vector3.right * y, ForceMode.Impulse);
+
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                int sign = 1;
+                var playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+                if (Input.mousePosition.x < playerScreenPoint.x)
+                {
+                    sign = -1;
+                    Debug.Log("mouse is to the left");
+                }
+                else
+                {
+                    Debug.Log("mouse is to the right");
+                }
+                rb.AddTorque(Vector3.forward * (y * sign), ForceMode.Impulse);
+            }
+
+            else
+            {
+                rb.AddTorque(Vector3.down * x, ForceMode.Impulse);
+                rb.AddTorque(Vector3.right * y, ForceMode.Impulse);
+            }
         }
     }
 }
