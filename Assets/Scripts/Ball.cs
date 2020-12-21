b8490f331f441218b8b7416c6ff4ac6c4fc33138
miriSilverman <mirisboom@gmail.com>
2020-12-21 14:04:35 +0200

    deleted extra

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -10,10 +10,11 @@ public class Ball : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
     [SerializeField] private GameObject room;
-
+    
+    
     private Rigidbody _rb;
-    
-    
+    private bool _inWindArea = false;
+    private WindArea _windArea;
     
     void Start()
     {
@@ -23,13 +24,36 @@ public class Ball : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        if (_inWindArea)
+        {
+            _rb.AddForce(_windArea.direction * _windArea.strength);
+        }
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
         
-        //_rb.AddForce(movement*speed);
+        _rb.AddForce(movement*speed);
     }
     private void OnMouseDown()
     {
         room.transform.DORotate(new Vector3(0, 0, 90), 10f);
 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("windArea"))
+        {
+            _inWindArea = true;
+            _windArea = other.gameObject.GetComponent<WindArea>();
+        }
+        
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("windArea"))
+        {
+            _inWindArea = false;
+        }
     }
 }
