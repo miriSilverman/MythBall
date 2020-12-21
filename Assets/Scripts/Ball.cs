b17459dd467b9b8b48681c61a1a4dd69053c558b
miriSilverman <mirisboom@gmail.com>
2020-12-21 10:51:55 +0200

    added wind zone

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -12,6 +12,9 @@ public class Ball : MonoBehaviour
     [SerializeField] private GameObject room;
 
     private Rigidbody _rb;
+    private bool _inWindArea;
+    private GameObject _windArea;
+    private WindArea _windAreaS;
     
     
     
@@ -23,6 +26,10 @@ public class Ball : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (_inWindArea)
+        {
+            _rb.AddForce(_windAreaS.direction * _windAreaS.strength);
+        }
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
         
         _rb.AddForce(movement*speed);
@@ -31,5 +38,25 @@ public class Ball : MonoBehaviour
     {
         room.transform.DORotate(new Vector3(0, 0, 90), 10f);
 
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("windArea"))
+        {
+            _inWindArea = true;
+            _windArea = other.gameObject;
+            _windAreaS = _windArea.GetComponent<WindArea>();
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("windArea"))
+        {
+            _inWindArea = false;
+        }
     }
 }
