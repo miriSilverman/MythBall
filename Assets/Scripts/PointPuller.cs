1f33e11d762b512ca6d9a9d0479e7ea348362734
miriSilverman <mirisboom@gmail.com>
2020-12-22 23:25:11 +0200

    added pulling prefab

--- Assets/Scripts/PointPuller.cs
+++ Assets/Scripts/PointPuller.cs
@@ -28,25 +28,17 @@ public class PointPuller : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        // ball = other.gameObject;
         if (other.gameObject.CompareTag("Ball"))
         {
             _inField = true;
-            // Vector3 v = transform.position - ball.transform.position;
-            // Rigidbody rb = ball.GetComponent<Rigidbody>();
-            // rb.AddForce(v.normalized * force);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        // ball = other.gameObject;
         if (other.gameObject.CompareTag("Ball"))
         {
             _inField = false;
-            // Vector3 v = transform.position - ball.transform.position;
-            // Rigidbody rb = ball.GetComponent<Rigidbody>();
-            // rb.AddForce(v.normalized * force);
         }
     }
 }
